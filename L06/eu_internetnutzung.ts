529f96843d0899e7af88ea78cb99a8494af8e785
Larahalmosi <lara.sophia.elisabeth.halmosi@hs-furtwangen.de>
2022-05-08 17:46:30 +0200

    add 06

---
+++ L06/eu_internetnutzung.ts
@@ -0,0 +1,74 @@
+var Prozent1:number=93
+console.log("Die Internetnutzung in Deutschland beträgt im Jahr 2022:"+Prozent1 +"%") ;
+
+var Dtl2022: number=93;
+var EU2022: number=88;
+
+let Prozent2=(Dtl2022-EU2022)-1
+
+console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Deutschland im Jahr 2022:"+ Prozent2+ " %") ;
+
+var Dtl2022: number=93;
+var Dtl2010: number=74;
+
+let Prozent3=(Dtl2022-Dtl2010)-1;
+
+console.log("Für Deutschland hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um " +Prozent3+" % verändert.") ;
+
+
+var Prozent4: number= 91;  
+
+console.log("Die Internetnutzung in Frankreich beträgt im Jahr 2022:"+Prozent4 +"%") ;
+
+var F2022: number=91;
+var EU2022: number=88;
+
+let Prozent5=(F2022-EU2022)-1;
+
+console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Frankreich im Jahr 2022:"+ Prozent5+ " %") ;
+
+var F2022: number=91;
+var F2010: number=75;
+
+let Prozent6=(F2022-F2010)-1;
+
+console.log("Für Frankreich hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2008 um " +Prozent6+" % verändert.") ;
+
+
+var Prozent7: number = 84;  
+
+console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Italien im Jahr 2022:"+ Prozent7+ " %") ;
+var I2022: number=84;
+var EU2022: number=88;
+
+let Prozent8=(I2022-EU2022)-1;
+
+console.log("Für Italien hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2008 um " +Prozent8+" % verändert.") ;
+var I2022: number=84;
+var I2010: number=48;
+
+let Prozent9=(I2022-I2010)-1;
+
+console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Italien im Jahr 2022:"+ Prozent9+ " %") ;
+
+
+var Prozent10: number = 81; 
+
+console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Rumanien im Jahr 2022:"+ Prozent10+ " %") ;
+var R2022: number=81;
+var EU2022: number=88;
+
+let Prozent11=(R2022-EU2022)-1;
+
+
+console.log("Für Rumanien hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2008 um " +Prozent11+" % verändert.") ;
+
+var R2022: number=81;
+var R2010: number=34;
+
+let Prozent12=(R2022-R2010)-1;
+
+console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Rumanien im Jahr 2022:"+ Prozent12+ " %") ;
+
+
+
