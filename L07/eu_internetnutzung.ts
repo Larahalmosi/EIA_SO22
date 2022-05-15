632a18beee8b85cede85e8dc0ae9de0c0963bb71
@y0urv39et4bles <101884905+Larahalmosi@users.noreply.github.com>
2022-05-15 18:23:29 +0200

    add

--- L07/eu_internetnutzung.ts
+++ L07/eu_internetnutzung.ts
@@ -2,9 +2,6 @@ function interdtl(Internetnutzung:number){
     var Prozent1:number=93
 console.log("Die Internetnutzung in Deutschland beträgt im Jahr 2022:"+Prozent1 +"%") ;
 }
-window.addEventListener("load",function(){
-    document.querySelector(".germany")addEventListener("click",interdtl)
-    });
 
 function interdtlzueu(Internetnutzung:number){
 var Dtl2022: number=93;
@@ -15,9 +12,6 @@ let Prozent2=(Dtl2022-EU2022)-1
 console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Deutschland im Jahr 2022:"+ Prozent2+ " %") ;
 }
 
-window.addEventListener("load",function(){
-    document.querySelector(".germany")addEventListener("click",interdtlzueu)
-    });
 
 function interdtlvergleich2010(Internetnutzung:number){
 
@@ -28,9 +22,7 @@ let Prozent3=(Dtl2022-Dtl2010)-1;
 
 console.log("Für Deutschland hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um " +Prozent3+" % verändert.") ;
 }
-window.addEventListener("load",function(){
-    document.querySelector(".germany")addEventListener("click",interdtlvergleich2010)
-    });
+
 
 function interfr(Internetnutzung:number){
 
@@ -39,9 +31,7 @@ var Prozent4: number= 91;
 console.log("Die Internetnutzung in Frankreich beträgt im Jahr 2022:"+Prozent4 +"%") ;
 }
 
-window.addEventListener("load",function(){
-    document.querySelector(".france")addEventListener("click",interfr)
-    });
+
 function interfrzueu(Internetnutzung:number){
 
 var F2022: number=91;
@@ -51,9 +41,7 @@ let Prozent5=(F2022-EU2022)-1;
 
 console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Frankreich im Jahr 2022:"+ Prozent5+ " %") ;
 }
-window.addEventListener("load",function(){
-    document.querySelector(".france")addEventListener("click",interfrzueu)
-    });
+
 
 function interfrvergleich2010(Internetnutzung:number){
 var F2022: number=91;
@@ -63,11 +51,15 @@ let Prozent6=(F2022-F2010)-1;
 
 console.log("Für Frankreich hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um " +Prozent6+" % verändert.") ;
 }
+
+
 function interit(Internetnutzung:number){
 var Prozent7: number = 84;  
 
 console.log("Die Internetnutzung in Italien beträgt im Jahr 2022:"+ Prozent7+ " %") ;
 }
+
+
 function interitzueu(Internetnutzung:number){
 var I2022: number=84;
 var EU2022: number=88;
@@ -76,6 +68,8 @@ let Prozent8=(I2022-EU2022)-1;
 
 console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Italien im Jahr 2022:" +Prozent8+" % verändert.") ;
 }
+
+
 function interitvergleich2010(Internetnutzung:number){
 var I2022: number=84;
 var I2010: number=48;
@@ -90,15 +84,18 @@ var Prozent10: number = 81;
 
 console.log("Die Internetnutzung in Rumänien beträgt im Jahr 2022:"+ Prozent10+ " %") ;
 }
+
+
 function interrumzueu(Internetnutzung:number){
 var R2022: number=81;
 var EU2022: number=88;
 
 let Prozent11=(R2022-EU2022)-1;
 
-
 console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Rumänien im Jahr 2022:"+Prozent11+" % verändert.") ;
 }
+
+
 function interrumvergleich2010(Internetnutzung:number){
 var R2022: number=81;
 var R2010: number=34;
@@ -107,9 +104,26 @@ let Prozent12=(R2022-R2010)-1;
 
 console.log("Für Rumänien hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um "+ Prozent12+ " %") ;
 }
+
+
 function eustars (88){
     console.log (88);
 } 
+
+document.querySelector(".internetnutzungland").innerHTML="Die Internetnutzung in Deutschland beträgt im Jahr 2022:"+Prozent1 +"%";
+
 window.addEventListener("load",function(){
-document.querySelector(".stars")addEventListener("click",eustars)
+     document.querySelector(".germany").addEventListener("click",interdtl);
+     document.querySelector(".germany").addEventListener("click",interdtlzueu);
+     document.querySelector(".germany").addEventListener("click",interdtlvergleich2010);
+     document.querySelector(".france").addEventListener("click",interfr);
+     document.querySelector(".france").addEventListener("click",interfrzueu);
+     document.querySelector(".france").addEventListener("click",interfrvergleich2010);
+     document.querySelector(".france").addEventListener("click",interit);
+     document.querySelector(".france").addEventListener("click",interitzueu);
+     document.querySelector(".france").addEventListener("click",interitvergleich2010);
+     document.querySelector(".france").addEventListener("click",interrum);
+     document.querySelector(".france").addEventListener("click",interrumzueu);
+     document.querySelector(".france").addEventListener("click",interrumvergleich2010);
+     document.querySelector(".stars").addEventListener("click",eustars);
 });
