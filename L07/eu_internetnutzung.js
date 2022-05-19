89c0d48e863886bf88b2dbaa1ee4383b4cdb9e4d
Larahalmosi <lara.sophia.elisabeth.halmosi@hs-furtwangen.de>
2022-05-19 21:37:44 +0200

    add

--- L07/eu_internetnutzung.js
+++ L07/eu_internetnutzung.js
@@ -1,92 +1,86 @@
-function interdtl(Internetnutzung) {
+function interdtl() {
     var Prozent1 = 93;
     console.log("Die Internetnutzung in Deutschland beträgt im Jahr 2022:" + Prozent1 + "%");
-}
-window.addEventListener("load", function () {
-    document.querySelector(".germany");
-    addEventListener("click", interdtl);
-});
-function interdtlzueu(Internetnutzung) {
+    document.querySelector(".internetnutzungsland").innerHTML = "Beträgt die Internetnutzung im Jahr 2022";
+    document.getElementById("pro-land").innerHTML = Prozent1;
     var Dtl2022 = 93;
     var EU2022 = 88;
     let Prozent2 = (Dtl2022 - EU2022) - 1;
     console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Deutschland im Jahr 2022:" + Prozent2 + " %");
-}
-window.addEventListener("load", function () {
-    document.querySelector(".germany");
-    addEventListener("click", interdtlzueu);
-});
-function interdtlvergleich2010(Internetnutzung) {
+    document.querySelector("gesamt").innerHTML = "Relativ zur Gesamtzahl der Internetnutzer in der EU";
+    document.getElementById("landzueu").innerHTML = Prozent2;
     var Dtl2022 = 93;
     var Dtl2010 = 74;
     let Prozent3 = (Dtl2022 - Dtl2010) - 1;
     console.log("Für Deutschland hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um " + Prozent3 + " % verändert.");
+    document.querySelector("unterschiedjahr").innerHTML = "Veränderung der Internetnutzung für Deutschland im Jahr 2022 im Vergleich zu 2010 ";
+    document.getElementById("veraenderungland2010").innerHTML = Prozent3;
 }
-window.addEventListener("load", function () {
-    document.querySelector(".germany");
-    addEventListener("click", interdtlvergleich2010);
-});
-function interfr(Internetnutzung) {
+function interfr() {
     var Prozent4 = 91;
     console.log("Die Internetnutzung in Frankreich beträgt im Jahr 2022:" + Prozent4 + "%");
-}
-window.addEventListener("load", function () {
-    document.querySelector(".france");
-    addEventListener("click", interfr);
-});
-function interfrzueu(Internetnutzung) {
+    document.querySelector(".internetnutzungsland").innerHTML = "Beträgt die Internetnutzung im Jahr 2022";
+    document.getElementById("pro-land").innerHTML = Prozent4;
     var F2022 = 91;
     var EU2022 = 88;
     let Prozent5 = (F2022 - EU2022) - 1;
     console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Frankreich im Jahr 2022:" + Prozent5 + " %");
-}
-window.addEventListener("load", function () {
-    document.querySelector(".france");
-    addEventListener("click", interfrzueu);
-});
-function interfrvergleich2010(Internetnutzung) {
+    document.querySelector(".internetnutzungsland").innerHTML = "Relativ zur Gesamtzahl der Internetnutzer in der EU";
+    document.getElementById("landzueu").innerHTML = Prozent5;
     var F2022 = 91;
     var F2010 = 75;
     let Prozent6 = (F2022 - F2010) - 1;
     console.log("Für Frankreich hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um " + Prozent6 + " % verändert.");
+    document.querySelector("unterschiedjahr").innerHTML = "Veränderung der Internetnutzung für Frankreich im Jahr 2022 im Vergleich zu 2010 ";
+    document.getElementById("veraenderungland2010").innerHTML = Prozent6;
 }
-function interit(Internetnutzung) {
+function interit() {
     var Prozent7 = 84;
     console.log("Die Internetnutzung in Italien beträgt im Jahr 2022:" + Prozent7 + " %");
-}
-function interitzueu(Internetnutzung) {
+    document.querySelector(".internetnutzungsland").innerHTML = "Beträgt die Internetnutzung im Jahr 2022";
+    document.getElementById("pro-land").innerHTML = Prozent7;
     var I2022 = 84;
     var EU2022 = 88;
     let Prozent8 = (I2022 - EU2022) - 1;
     console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Italien im Jahr 2022:" + Prozent8 + " % verändert.");
-}
-function interitvergleich2010(Internetnutzung) {
+    document.querySelector(".internetnutzungsland").innerHTML = "Relativ zur Gesamtzahl der Internetnutzer in der EU";
+    document.getElementById("landzueu").innerHTML = Prozent8;
     var I2022 = 84;
     var I2010 = 48;
     let Prozent9 = (I2022 - I2010) - 1;
     console.log("Für Italien hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um " + Prozent9 + " %");
+    document.querySelector("unterschiedjahr").innerHTML = "Veränderung der Internetnutzung für Italien im Jahr 2022 im Vergleich zu 2010 ";
+    document.getElementById("veraenderungland2010").innerHTML = Prozent9;
 }
-function interrum(Internetnutzung) {
+function interrum() {
     var Prozent10 = 81;
     console.log("Die Internetnutzung in Rumänien beträgt im Jahr 2022:" + Prozent10 + " %");
-}
-function interrumzueu(Internetnutzung) {
+    document.querySelector(".internetnutzungsland").innerHTML = "Beträgt die Internetnutzung im Jahr 2022";
+    document.getElementById("pro-land").innerHTML = Prozent10;
     var R2022 = 81;
     var EU2022 = 88;
     let Prozent11 = (R2022 - EU2022) - 1;
     console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Rumänien im Jahr 2022:" + Prozent11 + " % verändert.");
-}
-function interrumvergleich2010(Internetnutzung) {
+    document.querySelector(".internetnutzungsland").innerHTML = "Relativ zur Gesamtzahl der Internetnutzer in der EU";
+    document.getElementById("landzueu").innerHTML = Prozent11;
     var R2022 = 81;
     var R2010 = 34;
     let Prozent12 = (R2022 - R2010) - 1;
     console.log("Für Rumänien hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um " + Prozent12 + " %");
+    document.querySelector("unterschiedjahr").innerHTML = "Veränderung der Internetnutzung für Rumänien im Jahr 2022 im Vergleich zu 2010 ";
+    document.getElementById("veraenderungland2010").innerHTML = Prozent12;
 }
 function eustars() {
     console.log(88);
+    var EU2022 = 88;
+    document.querySelector(".internetnutzungsland").innerHTML = "Die Internetnutzung in Europa beträgt im Jahr 2022:" + EU2022 + "%";
+    document.querySelector(".stars").innerHTML = eustars;
 }
 window.addEventListener("load", function () {
-    document.querySelector(".stars");
-    addEventListener("click", eustars);
+    document.querySelector(".germany").addEventListener("click", interdtl);
+    document.querySelector(".france").addEventListener("click", interfr);
+    document.querySelector(".italy").addEventListener("click", interit);
+    document.querySelector(".romania").addEventListener("click", interrum);
+    document.querySelector(".stars").addEventListener("click", eustars);
 });
 //# sourceMappingURL=eu_internetnutzung.js.map
--- L07/eu_internetnutzung.ts
+++ L07/eu_internetnutzung.ts
@@ -1,12 +1,11 @@
-function interdtl(Internetnutzung:number){
+function interdtl(){
+
     var Prozent1:number=93
 console.log("Die Internetnutzung in Deutschland beträgt im Jahr 2022:"+Prozent1 +"%") ;
 
-document.querySelector(".internetnutzungsland").innerHTML="Die Internetnutzung in Deutschland beträgt im Jahr 2022:"+Prozent1 +"%"
-document.getElementById("Internetnutzung:number").innerHTML=interdtl;
-}
+document.querySelector(".internetnutzungsland").innerHTML="Beträgt die Internetnutzung im Jahr 2022"
+document.getElementById("pro-land").innerHTML= Prozent1;
 
-function interdtlzueu(Internetnutzung:number){
 
 var Dtl2022: number=93;
 var EU2022: number=88;
@@ -14,12 +13,10 @@ var EU2022: number=88;
 let Prozent2=(Dtl2022-EU2022)-1
 
 console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Deutschland im Jahr 2022:"+ Prozent2+ " %") ;
-document.querySelector(".internetnutzungsland").innerHTML="Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Deutschland im Jahr 2022:"+Prozent2 +"%"
-document.getElementById("Internetnutzung:number").innerHTML=interdtlzueu;
-}}
+document.querySelector("gesamt").innerHTML="Relativ zur Gesamtzahl der Internetnutzer in der EU"
+document.getElementById("landzueu").innerHTML=Prozent2;
 
 
-function interdtlvergleich2010(Internetnutzung:number){
 
 var Dtl2022: number=93;
 var Dtl2010: number=74;
@@ -27,22 +24,19 @@ var Dtl2010: number=74;
 let Prozent3=(Dtl2022-Dtl2010)-1;
 
 console.log("Für Deutschland hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um " +Prozent3+" % verändert.") ;
-document.querySelector(".internetnutzungsland").innerHTML="Für Deutschland hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um:"+Prozent3 +"% verändert."
-document.getElementById("Internetnutzung:number").innerHTML=interdtlvergleich2010;
+document.querySelector("unterschiedjahr").innerHTML="Veränderung der Internetnutzung für Deutschland im Jahr 2022 im Vergleich zu 2010 "
+document.getElementById("veraenderungland2010").innerHTML=Prozent3;
 }
 
 
-function interfr(Internetnutzung:number){
+function interfr(){
 
 var Prozent4: number= 91;  
 
 console.log("Die Internetnutzung in Frankreich beträgt im Jahr 2022:"+Prozent4 +"%") ;
-document.querySelector(".internetnutzungsland").innerHTML="Die Internetnutzung in Frankreich beträgt im Jahr 2022:"+Prozent4 +"%"
-document.getElementById("Internetnutzung:number").innerHTML=interfr;
-}
+document.querySelector(".internetnutzungsland").innerHTML="Beträgt die Internetnutzung im Jahr 2022"
+document.getElementById("pro-land").innerHTML=Prozent4;
 
-
-function interfrzueu(Internetnutzung:number){
 
 var F2022: number=91;
 var EU2022: number=88;
@@ -50,109 +44,93 @@ var EU2022: number=88;
 let Prozent5=(F2022-EU2022)-1;
 
 console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Frankreich im Jahr 2022:"+ Prozent5+ " %") ;
-document.querySelector(".internetnutzungsland").innerHTML="Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Frankreich im Jahr 2022:"+Prozent5 +"%"
-document.getElementById("Internetnutzung:number").innerHTML=interdtl;
-}
+document.querySelector(".internetnutzungsland").innerHTML="Relativ zur Gesamtzahl der Internetnutzer in der EU"
+document.getElementById("landzueu").innerHTML=Prozent5;
 
 
-function interfrvergleich2010(Internetnutzung:number){
+
 var F2022: number=91;
 var F2010: number=75;
 
 let Prozent6=(F2022-F2010)-1;
 
 console.log("Für Frankreich hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um " +Prozent6+" % verändert.") ;
-document.querySelector(".internetnutzungsland").innerHTML="Für Frankreich hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um:"+Prozent6 +"% verändert."
-document.getElementById("Internetnutzung:number").innerHTML=interfrvergleich2010;
+document.querySelector("unterschiedjahr").innerHTML="Veränderung der Internetnutzung für Frankreich im Jahr 2022 im Vergleich zu 2010 "
+document.getElementById("veraenderungland2010").innerHTML=Prozent6;
 }
 
 
-function interit(Internetnutzung:number){
+function interit(){
 var Prozent7: number = 84;  
 
 console.log("Die Internetnutzung in Italien beträgt im Jahr 2022:"+ Prozent7+ " %") ;
-document.querySelector(".internetnutzungsland").innerHTML="Die Internetnutzung in Italien beträgt im Jahr 2022:"+Prozent7 +"%"
-document.getElementById("Internetnutzung:number").innerHTML=interit;
-}
+document.querySelector(".internetnutzungsland").innerHTML="Beträgt die Internetnutzung im Jahr 2022"
+document.getElementById("pro-land").innerHTML=Prozent7;
 
 
-function interitzueu(Internetnutzung:number){
+
+
 var I2022: number=84;
 var EU2022: number=88;
 
 let Prozent8=(I2022-EU2022)-1;
 
 console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Italien im Jahr 2022:" +Prozent8+" % verändert.") ;
-document.querySelector(".internetnutzungsland").innerHTML="Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Italien im Jahr 2022:"+Prozent8 +"%"
-document.getElementById("Internetnutzung:number").innerHTML=interitzueu;
-}
+document.querySelector(".internetnutzungsland").innerHTML="Relativ zur Gesamtzahl der Internetnutzer in der EU"
+document.getElementById("landzueu").innerHTML=Prozent8;
 
 
-function interitvergleich2010(Internetnutzung:number){
+
 var I2022: number=84;
 var I2010: number=48;
 
 let Prozent9=(I2022-I2010)-1;
 
 console.log("Für Italien hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um "+ Prozent9+ " %") ;
-document.querySelector(".internetnutzungsland").innerHTML="Für Italien hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um:"+Prozent9 +"% verändert."
-document.getElementById("Internetnutzung:number").innerHTML=interitvergleich2010;
+document.querySelector("unterschiedjahr").innerHTML="Veränderung der Internetnutzung für Italien im Jahr 2022 im Vergleich zu 2010 "
+document.getElementById("veraenderungland2010").innerHTML=Prozent9;
 }
 
-function interrum(Internetnutzung:number){
+function interrum(){
 var Prozent10: number = 81; 
 
 console.log("Die Internetnutzung in Rumänien beträgt im Jahr 2022:"+ Prozent10+ " %") ;
-document.querySelector(".internetnutzungsland").innerHTML="Die Internetnutzung in Rumänien beträgt im Jahr 2022:"+Prozent1 +"%"
-document.getElementById("Internetnutzung:number").innerHTML=interrum;
-}
+document.querySelector(".internetnutzungsland").innerHTML="Beträgt die Internetnutzung im Jahr 2022"
+document.getElementById("pro-land").innerHTML=Prozent10;
 
-function interrumzueu(Internetnutzung:number){
 var R2022: number=81;
 var EU2022: number=88;
 
 let Prozent11=(R2022-EU2022)-1;
 
 console.log("Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Rumänien im Jahr 2022:"+Prozent11+" % verändert.") ;
-document.querySelector(".internetnutzungsland").innerHTML="Relativ zur Gesamtzahl der Internetnutzer in der EU ist die Internetnutzung in Frankreich im Jahr 2022:"+Prozent11 +"%"
-document.getElementById("Internetnutzung:number").innerHTML=interrumzueu;
-}
+document.querySelector(".internetnutzungsland").innerHTML="Relativ zur Gesamtzahl der Internetnutzer in der EU"
+document.getElementById("landzueu").innerHTML=Prozent11;
 
-
-function interrumvergleich2010(Internetnutzung:number){
 var R2022: number=81;
 var R2010: number=34;
 
 let Prozent12=(R2022-R2010)-1;
 
 console.log("Für Rumänien hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um "+ Prozent12+ " %") ;
-document.querySelector(".internetnutzungsland").innerHTML="Für Rumänien hat sich die Internetnutzung im Jahr 2022 im Vergleich zu 2010 um:"+Prozent12 +"% verändert."
-document.getElementById("Internetnutzung:number").innerHTML=interrumvergleich2010;
+document.querySelector("unterschiedjahr").innerHTML="Veränderung der Internetnutzung für Rumänien im Jahr 2022 im Vergleich zu 2010 "
+document.getElementById("veraenderungland2010").innerHTML=Prozent12;
 }
 
 
-function eustars (88){
+function eustars (){
     console.log (88);
 
+    var EU2022: number=88
 
-document.querySelector(".internetnutzungsland").innerHTML="Die Internetnutzung in Deutschland beträgt im Jahr 2022:"+Prozent1 +"%"
-document.getElementById("Internetnutzung:number").innerHTML=eustars;
+document.querySelector(".internetnutzungsland").innerHTML="Die Internetnutzung in Europa beträgt im Jahr 2022:"+EU2022 +"%"
+document.querySelector(".stars").innerHTML=eustars;
 }
 
 window.addEventListener("load",function(){
      document.querySelector(".germany").addEventListener("click",interdtl);
-     document.querySelector(".germany").addEventListener("click",interdtlzueu);
-     document.querySelector(".germany").addEventListener("click",interdtlvergleich2010);
      document.querySelector(".france").addEventListener("click",interfr);
-     document.querySelector(".france").addEventListener("click",interfrzueu);
-     document.querySelector(".france").addEventListener("click",interfrvergleich2010);
-     document.querySelector(".france").addEventListener("click",interit);
-     document.querySelector(".france").addEventListener("click",interitzueu);
-     document.querySelector(".france").addEventListener("click",interitvergleich2010);
-     document.querySelector(".france").addEventListener("click",interrum);
-     document.querySelector(".france").addEventListener("click",interrumzueu);
-     document.querySelector(".france").addEventListener("click",interrumvergleich2010);
+     document.querySelector(".italy").addEventListener("click",interit);
+     document.querySelector(".romania").addEventListener("click",interrum);
      document.querySelector(".stars").addEventListener("click",eustars);
 });
-
-
