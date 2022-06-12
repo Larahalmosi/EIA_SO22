9bf991aea584aab9213e16871b82ebbef07ca46e
@y0urv39et4bles <101884905+Larahalmosi@users.noreply.github.com>
2022-06-12 18:02:08 +0200

    add

--- L10/script.js
+++ L10/script.js
@@ -1,11 +1,18 @@
 var let, total = 0;
 let oben = 0;
 let done = 0;
-let add, Taskbox = document.getElementbyID("addTask, ");, let, newDiv, HTMLElement = document.createElement("div"));
+let add, Taskbox = document.getElementbyID("addTask");
+let newDiv = document.createElement("div");
 document.querySelector("#addBtn").addEventListener("click", addNewTask);
 total++;
 document.querySelector("#myinput").innerHTML = String(total + " in total");
 let textEingabefeld = document.createElement("h7");
 textEingabefeld.innerHTML = input.value;
 textEingabefeld.id = "text";
+//Entertaste//
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode == 13) {
+        addNewTask();
+    }
+});
 //# sourceMappingURL=script.js.map
--- L10/script.ts
+++ L10/script.ts
@@ -4,8 +4,7 @@ let total :number=0;
 let oben: number=0;
 let done:number=0;
 
-let add Taskbox: HTMLElement=  document.getElementbyID("addTask
-");
+let add Taskbox: HTMLElement=  document.getElementbyID("addTask");
 let newDiv: HTMLElement= document.createElement("div");
 
 document.querySelector("#addBtn").addEventListener("click", addNewTask);
@@ -16,3 +15,9 @@ let textEingabefeld: HTMLElement = document.createElement("h7");
 
 textEingabefeld.innerHTML = input.value;
 textEingabefeld.id = "text";
+//Entertaste//
+document.addEventListener("keydown", function (event: KeyboardEvent): void { 
+    if (event.keyCode == 13) {
+       addNewTask();
+    }
+});
